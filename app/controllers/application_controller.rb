f0c0baa4bb3b5a15889b6cabbf5b79eee9c714cd
bradsloan <email": null>
2017-11-09 13:36:12 -0500

    add user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+  before_action :current_user
+
+   def current_user
+     @current_user ||= User.find_by(id: session[:user_id])
+   end
+
+   def login?
+     !!current_user
+   end
+
+   def require_user
+     redirect_to '/login' unless login?
+   end
+
 end
