2e3f257a96dcbda368409431e355ca9e184e8b8c
Najah Izquierdo <najah.izquierdo@yahoo.com>
2017-11-09 17:17:48 -0500

    Fix migrations

--- mediamine-react/src/App.js
+++ mediamine-react/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
+      <Router>
+        <Link to={'/login'}>
+        <h1> Hello! </h1>
+      </Link>
+    </Router>
 
     );
   }
