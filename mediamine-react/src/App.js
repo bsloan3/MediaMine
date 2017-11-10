69a7df78841b4134e4f77b98aa6dcb3e20f5d2a4
bradsloan <email": null>
2017-11-09 23:13:24 -0500

    seed 72 podcasts

--- mediamine-react/src/App.js
+++ mediamine-react/src/App.js
@@ -19,10 +19,9 @@ class App extends Component {
       <h1>dgsfsdgf</h1>
       <h1>dgsfsdgf</h1>
       <h1>dgsfsdgf</h1>
-      <h1>dgsfsdgf</h1>
-      <h1>dgsfsdgf</h1>
-      <h1>dgsfsdgf</h1>
-      <h1>dgsfsdgf</h1>
+      <h1><a href="">ggg</a></h1>
+      <iframe width="25%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/321908803&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe>
+<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/175240992&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
       <h1>dgsfsdgf</h1>
       <h1>dgsfsdgf</h1>
       <h1>dgsfsdgf</h1>
