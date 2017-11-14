7076678d102aaa12485352a7434b72683b0050e0
bradsloan <email": null>
2017-11-14 18:27:23 -0500

    popup

--- mediamine-react/src/containers/MusicContainer.js
+++ mediamine-react/src/containers/MusicContainer.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
 export default class MusicContainer extends Component {
+  constructor(){
+    super();
+    this.state = {
+      display_class: 'hide'
+    }
+  }
+
+  hide(){
+    let display_class;
+    this.state.display_class === 'player' ? display_class = 'hide' : display_class = 'player';
+    console.log(display_class)
+    this.setState({display_class})
+  }
   render() {
     return (
       <div className="page_container">
-
-        <h1 style={{paddingRight: '5%', paddingTop: '4%', color: 'white', textAlign: 'right'}}> Music </h1>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLeqkFcL9xTiDnqU0aE10_qddAG9aSPtKJ" frameborder="0" gesture="media" allowfullscreen></iframe>
+        <div id="player" style={{marginBottom: '1%', marginLeft: '1%'}} className={this.state.display_class}>
+          <iframe src="https://www.youtube.com/embed/videoseries?list=PLeqkFcL9xTiDnqU0aE10_qddAG9aSPtKJ" frameborder="0" gesture="media" allowfullscreen></iframe>
+        </div>
+        <input onClick={this.hide.bind(this)} style={{position: 'fixed', bottom: '0', marginBottom: '1%', marginLeft: '1%'}} type="submit" value="MUSIC"/>
       </div>
     );
   }
