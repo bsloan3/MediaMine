9b56800f2d724c7928964fd0132c11b84db9a7b8
bradsloan <email": null>
2017-11-16 15:21:29 -0500

    final seed and style

--- mediamine-react/src/containers/MoviesComponents/MovieInformation.js
+++ mediamine-react/src/containers/MoviesComponents/MovieInformation.js
@@ -7,7 +7,7 @@ export default class MovieInformation extends Component {
   render() {
     let {movie, addMovie} = this.props
     return(
-      <div>
+      <div  style={{color: 'white'}}>
       <center><h2>{movie.title}</h2>
       <img className="img" src= {'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + movie.poster_path} />
       <p>{movie.release_date}</p>
