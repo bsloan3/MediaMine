a4a79420393c0aad5cb5b86c8d9e8f10980e9385
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-11-15 21:54:24 -0500

    Add event form

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get '/users/:id/sports', to: 'sports#index'
   get '/podcasts', to:'podcasts#home'
   get '/users/:id/calendarevents', to: 'calendar_events#index'
-
+  get '/users/:id/events', to: 'calendar_events#create'
   post '/users/:id/sports', to: 'interests#addsport'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/login', to:'sessions#new'
